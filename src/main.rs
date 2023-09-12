d031f28b53eb5b9dca41d36a8ae5447e27ab004f
YasyaKarasu <oycy0306@hotmail.com>
2023-09-13 00:39:13 +0800

    [FIX] remove useless mini-redis client

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 #![feature(impl_trait_in_assoc_type)]
 
-use lazy_static::lazy_static;
-use std::net::SocketAddr;
 use axum::{routing::*, Router};
 mod volo_gen;
 mod handler;
-
-lazy_static! {
-    static ref CLIENT: crate::volo_gen::volo_gen::mini::redis::MiniRedisServiceClient = {
-        let addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
-        crate::volo_gen::volo_gen::mini::redis::MiniRedisServiceClientBuilder::new("mini-redis")
-            .address(addr)
-            .build()
-    };
-}
 
 #[tokio::main]
 async fn main() {
